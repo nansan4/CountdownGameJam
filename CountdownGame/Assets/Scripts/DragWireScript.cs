8f1ce854cff21fbaa26b1df61321a3b400c19365
Liam <lking122@outlook.com>
2026-07-25 12:23:47 -0600

    wire dragging

--- CountdownGame/Assets/Scripts/DragWireScript.cs
+++ CountdownGame/Assets/Scripts/DragWireScript.cs
@@ -7,13 +7,27 @@ public class DragWireScript : MonoBehaviour
     public float bendStrength = 1;
     public LineRenderer lineRenderer;
 
+    private CircleCollider2D endColl;
+
+    private void Start()
+    {
+        //OnMouseDrag is called via event when a GUIElement or Collider is clicked, so we need a collider
+        //  that the player will click on to have that event be called
+        endColl = gameObject.AddComponent<CircleCollider2D>();
+        endColl.radius = 0.5f;
+        endColl.offset = P2.transform.position;
+    }
+
     private void OnMouseDrag()
     {
         // if (ToolManager.Instance.currentTool != CurrentTool.Pliers) return;
         List<Vector3> points = new List<Vector3>();
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        //place p2 at the midpoint of the line to use in bending code below
         P2.transform.position = new Vector3((transform.position.x + mousePos.x) / 2, (transform.position.y + mousePos.y) / 2 + bendStrength, 0);
 
+        //lerp positions of points to bend line
         for (float i = 0; i <= 1; i += 1 / vertexCount)
         {
             Vector3 t1 = Vector3.Lerp(transform.position, P2.position, i);
@@ -24,5 +38,11 @@ public class DragWireScript : MonoBehaviour
 
         lineRenderer.positionCount = points.Count;
         lineRenderer.SetPositions(points.ToArray());
+
+        
+        //set the collider to be at the end of the line
+        //I'm not sure if I should use mousePos, since there could be an edge case where the collider gets set away from the end of the cable
+        endColl.offset = new Vector3((transform.position.x + mousePos.x), (transform.position.y + mousePos.y), 0);
+
     }
 }
