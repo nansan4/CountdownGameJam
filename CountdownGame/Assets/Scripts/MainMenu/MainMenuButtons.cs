16c1c5a340c1b2cce3dba3f02c302304cbb74ed2
nansan4 <nando@santosalliances.com>
2026-07-25 22:06:58 -0600

    main menu

--- CountdownGame/Assets/Scripts/MainMenu/MainMenuButtons.cs
+++ CountdownGame/Assets/Scripts/MainMenu/MainMenuButtons.cs
@@ -1,11 +1,62 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
 
 public class MainMenuButtons : MonoBehaviour
 {
+
+    [SerializeField] private Button PlayButton;
+    [SerializeField] private Button QuitButton;
+    [SerializeField] private Image fadeOutImage;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        PlayButton.onClick.AddListener(StartGame);
+        QuitButton.onClick.AddListener(QuitGame);
+    }
+
+    private IEnumerator FadeInPlay()
+    {
+        yield return new WaitForSeconds(1f);
+        float fadeDuration = 1f;
+        float elapsedTime = 0f;
+        Color color = fadeOutImage.color;
+        while (elapsedTime < fadeDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            color.a = Mathf.Clamp01(elapsedTime / fadeDuration);
+            fadeOutImage.color = color;
+            yield return null;
+        }
+        SceneManager.LoadScene(1);
+    }
+    private IEnumerator FadeInQuit()
+    {
+        yield return new WaitForSeconds(1f);
+        float fadeDuration = 1f;
+        float elapsedTime = 0f;
+        Color color = fadeOutImage.color;
+        while (elapsedTime < fadeDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            color.a = Mathf.Clamp01(elapsedTime / fadeDuration);
+            fadeOutImage.color = color;
+            yield return null;
+        }
+        Application.Quit();
+    }
+    private void StartGame()
+    {
+        StartCoroutine(FadeInPlay());
         
+    }
+
+    private void QuitGame()
+    {
+
+        Debug.Log("Goodbye");
+        StartCoroutine(FadeInQuit());
     }
 
     // Update is called once per frame
